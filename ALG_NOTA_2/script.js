4f6376c6bd5d93a49124aa2ff5215a215f6e541d
ENZO GONCALVES SANTOS <enzo.gsantos@senacsp.edu.br>
2024-09-25 15:32:14 -0300

    primeiro commit

---
+++ ALG_NOTA_2/script.js
@@ -0,0 +1,9 @@
+let Nota = parseInt(prompt("Insira sua nota"));
+
+if (Nota >=7) {
+    alert("Aprovado")
+    }else if (Nota >= 5) {
+        alert ("Recuperação")
+    }else{
+        alert("Reprovado")
+     }
